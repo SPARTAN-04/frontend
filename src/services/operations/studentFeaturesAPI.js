c2e000f440d39f2b5c71886263c7a234579ad248
SPARTAN-04 <kavyasenop@gmail.com>
2025-09-06 00:07:48 +0530

    Update studentFeaturesAPI.js

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -43,6 +43,7 @@ export async function buyCourse(token, courses, userDetails, navigate, dispatch)
                                 })
 
         if(!orderResponse.data.success) {
+            console.log("DATA ERROR"),
             throw new Error(orderResponse.data.message);
         }
         console.log("PRINTING orderResponse", orderResponse);
