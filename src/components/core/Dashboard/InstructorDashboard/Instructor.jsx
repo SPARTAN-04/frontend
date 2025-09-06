1cc5e565a17c9dbbf7349eb95b1bb2f2a5331888
SPARTAN-04 <kavyasenop@gmail.com>
2025-09-06 19:00:52 +0530

    Update Instructor.jsx

--- src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -1,3 +1,10 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { fetchInstructorCourses } from '../../../../services/operations/courseDetailsAPI';
+import { getInstructorData } from '../../../../services/operations/profileAPI';
+import InstructorChart from './InstructorChart';
+import { Link } from 'react-router-dom';
+
 export default function Instructor() {
   const { token } = useSelector((state) => state.auth)
   const { user } = useSelector((state) => state.profile)
@@ -131,4 +138,3 @@ export default function Instructor() {
     </div>
   )
 }
-
